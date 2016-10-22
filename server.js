0208e317be8ae84519f5e2b4e72ab876518a4918
Nikhil Shagrithaya <nikhilshagri@gmail.com>
2016-10-22 20:26:59 +0530

    Added code to send ajax requests

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname+'/index.html');
 });
 
+app.post('/recipe-add', (req, res) => {
+	console.log('got a post req!', req);
+	res.send('successful!');
+});
+
 app.listen(3000, function () {
   console.log('Food Geek server listening on port 3000!');
 });
